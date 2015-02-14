4bebeacbb171b4c2c0579bf603ef790ef713593a
Neal Shyam <neal@audioshocker.com>
2015-02-14 16:28:48 -0500

    example keyfile

---
+++ keys_ex.py
@@ -0,0 +1,12 @@
+# NETFLIX LOGIN
+username = 'foo@b.ar'
+password = 's3cr3t'
+
+phone = '+15558675309' # for Twilio / SMS notifications
+#email = 'foo@b.ar' # for Sendgrid / email notifications?
+
+# TWILIO VARS
+sid = '-----------------------'
+token = '----------'
+fnum = '+15551112222'
+url = 'http://media.giphy.com/media/sebYJNgVydzP2/giphy.gif'
