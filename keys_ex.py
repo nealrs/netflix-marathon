917e2c725edd08d141ef21e1c91f2f11cb2746f6
Neal Shyam <neal@audioshocker.com>
2015-02-16 23:12:38 -0500

    twitter integration / absolute paths / more condition checking

--- keys_ex.py
+++ keys_ex.py
@@ -4,9 +4,16 @@ password = 's3cr3t'
 
 phone = '+15558675309' # your cell number for Twilio / SMS notifications
 #email = 'foo@b.ar' # for Sendgrid / email notifications?
+tu = 'twitterHandle' # twitter handle
 
 # TWILIO API https://www.twilio.com/user/account/voice-messaging
 sid = '-----------------------'
 token = '----------'
 fnum = '+1888865309' #no seriously, your twilio # goes here
 url = 'http://media.giphy.com/media/gx656616VR5ew/giphy.gif' #this can be '' if you don't want a gif
+
+# TWITTER API
+ck = '-------------------------'
+cs = '-------------------------'
+atk = '-------------------------'
+ats = '-------------------------'
--- nflx.py
+++ nflx.py
@@ -9,11 +9,12 @@ from bs4 import BeautifulSoup
 from keys import *
 from twilio.rest import TwilioRestClient
 from tinydb import TinyDB, where
+import twitter
 
 def run():
   #let's log in
   print('> Logging in as: '+ username)
-  b = webdriver.PhantomJS()
+  b = webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs')
   b.get('https://www.netflix.com/Login?locale=en-US')
   b.find_element_by_id('email').send_keys(username)
   b.find_element_by_id('password').send_keys(password)
@@ -31,8 +32,8 @@ def run():
   print '> Getting recent activity'
 
   n = datetime.date.today()
-  ds = '1/24/15' # fake flag for my account so I can trigger alert
-  #ds = str(n.month) + "/" + str(n.day) + "/" + str(n.year)[2:]
+  #ds = '1/24/15' # fake flag for my account so I can trigger alert
+  ds = str(n.month) + "/" + str(n.day) + "/" + str(n.year)[2:]
 
   s = BeautifulSoup(b.page_source)
   a=[] # array for show titles
@@ -44,7 +45,7 @@ def run():
           a.append(t.text)
 
   # If the last 3 episodes watched were from the same series -- it's probably a marathon -- oh and make sure we didn't already trigger an alert today for this  show.
-  if a[0] == a[1] and a[0] == a[2] and not checkDB(ds, username, a[0]):
+  if a[0] == a[1] and a[0] == a[2] and not checkDB(ds, username, a[0]) and a[1] and a[2]:
 
       #set flag in db
       updateDB(ds, username, a[0])
@@ -56,20 +57,24 @@ def run():
       # TWILIO SMS/MMS & OTHER HOOKS / ACTIONS
       smsMms(m)
 
+      # TWITTER SHAME!
+      tweet(a[0])
+
   else:
       # whatever, we already triggered an alert for this marathon condition
-      print '> Yeah yeah, you love ' + str(a[0]) + '.'
+      #print '> Yeah yeah, you love ' + str(a[0]) + '.'
+      pass
 
   b.quit()
 
 def checkDB(date,user,show):
-    db = TinyDB('db.json')
+    db = TinyDB('/users/neal/sites/netflix/db.json')
     t = db.table('log')
     r = t.search((where('date') == date) & (where('user') == user) & (where('show') == show))
     return r
 
 def updateDB(date,user,show):
-    db = TinyDB('db.json')
+    db = TinyDB('/users/neal/sites/netflix/db.json')
     t = db.table('log')
     t.insert({'date': date, 'user': user, 'show': show})
     #print t.all()
@@ -83,6 +88,10 @@ def smsMms(m):
         print "> sending SMS"
         tw = c.messages.create(to=phone, from_=fnum, body=m)
 
+def tweet(m):
+    api = twitter.Api(consumer_key=ck, consumer_secret=cs, access_token_key=atk, access_token_secret=ats)
+    s = api.PostUpdate('Hey @'+tu+' stop binge watching '+m+'!' )
+    print s.text
 
 if __name__ == '__main__':
   run()
